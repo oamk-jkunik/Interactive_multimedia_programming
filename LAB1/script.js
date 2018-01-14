64bd923964a2a02a062feb7fba6d7d770df9b56a
oamk-jkunik <35426590+oamk-jkunik@users.noreply.github.com>
2018-01-14 20:21:32 +0200

    Add files via upload

---
+++ LAB1/script.js
@@ -0,0 +1,5 @@
+function addPerson(form) {
+  var nameOfThePerson = form.nameOfThePerson.value;
+  var list = document.getElementById("PersonList");
+  list.innerHTML += "</p>" + nameOfThePerson + "</p>";
+}
